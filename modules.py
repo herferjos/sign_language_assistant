debe901230ab2fa0b0da8fa521231d132564c96b
herferjos <120600448+herferjos@users.noreply.github.com>
2023-12-10 12:12:38 +0100

    Update modules.py

--- modules.py
+++ modules.py
@@ -3,6 +3,12 @@ import torch
 from transformers import AutoModelForImageClassification, AutoImageProcessor
 from PIL import Image
 import os
+from openai import OpenAI
+import streamlit as st
+from gtts import gTTS
+from io import BytesIO
+
+client = OpenAI(api_key = st.secrets['api_key'])
 
 @st.cache_resource
 def load_model():
@@ -57,4 +63,24 @@ def text2image(text):
 
     return resulting_images
 
+def audio2text(file_path):
+    with open(file_path, "rb") as audio_file:
+        transcript_response = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=audio_file
+        )
+        # Accede al texto de la transcripción directamente desde el objeto de respuesta
+        return transcript_response.text
 
+def text2audio(text_to_speak):
+  
+    # Genera el archivo MP3
+    mp3_fp = BytesIO()
+    tts = gTTS(text_to_speak, lang='es')
+    tts.write_to_fp(mp3_fp)
+    
+    # Guarda el archivo MP3 en el sistema de archivos
+    mp3_fp.seek(0)
+    mp3_path = "generated_audio.mp3"
+    with open(mp3_path, "wb") as mp3_file:
+        mp3_file.write(mp3_fp.read())
