7c7bfa94d14f85d87bf2936449ed91eb1e686718
herferjos <120600448+herferjos@users.noreply.github.com>
2023-12-10 13:08:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,17 @@ if 'predictions' not in st.session_state:
     st.session_state.predictions = ""
 
 if st.button("Start Webcam"):
-    cap = cv2.VideoCapture(0)
+    st.session_state.cap = cv2.VideoCapture(0)
 
 stop_button = st.button("Stop Webcam")
 
-while cap is not None and stop_button is False:
-    ret, frame = cap.read()
-
+while st.session_state.cap is not None and stop_button is False:
+    ret, frame = st.session_state.cap.read()
+    
     st.session_state.predictions = st.session_state.predictions + (image2text(frame))
 
 if stop_button is True:
-    cap.release()
+    st.session_state.cap.release()
     st.write(st.session_state.predictions)
 
     if st.button("Restart"):
